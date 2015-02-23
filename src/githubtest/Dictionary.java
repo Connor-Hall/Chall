2b2ed64269d23b7f50bf1f4f70c2c94be0a5750e
1203549 <1203549@RG43012.rgu.ac.uk>
2015-02-23 10:28:33 +0000

    Basic Application with <Main(), Customer, Person and Visitor classes

---
+++ src/githubtest/Dictionary.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author 1203549
+ * @Date 23/02/15
+ */
+public class Dictionary {
+    
+    private String Definition;
+    
+    
+    public String getDefinition() {
+        return Definition;
+    }
+    
+    
+}
---
+++ src/githubtest/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author 1203549
+ * @Date 23/02/15
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
